b5ef9f47ea737b3967798a86b59cc9b4c48b60a8
Lester063 <102047666+Lester063@users.noreply.github.com>
2023-11-05 14:53:20 -0800

    Auth API
    Auth API.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\Api\StudentController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,9 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', [StudentController::class,'index']);
-Route::post('students', [StudentController::class,'store']);
-Route::get('students/{id}', [StudentController::class,'show']);
-Route::get('students/{id}/edit', [StudentController::class,'edit']);
-Route::put('students/{id}/edit', [StudentController::class,'update']);
-Route::delete('students/{id}/delete', [StudentController::class,'delete']);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    
+    //student endpoint
+    Route::get('students', [StudentController::class,'index']);
+    Route::post('students', [StudentController::class,'store']);
+    Route::get('students/{id}', [StudentController::class,'show']);
+    Route::get('students/{id}/edit', [StudentController::class,'edit']);
+    Route::put('students/{id}/edit', [StudentController::class,'update']);
+    Route::delete('students/{id}/delete', [StudentController::class,'delete']);
+});
