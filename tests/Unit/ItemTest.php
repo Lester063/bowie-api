7b3369d591a549895b88b42a85cef613102ec53d
Lester063 <102047666+Lester063@users.noreply.github.com>
2024-10-13 22:19:00 -0800

    add unit test for item
    unit tests for item negative scenario

--- tests/Unit/ItemTest.php
+++ tests/Unit/ItemTest.php
@@ -31,4 +31,135 @@ class ItemTest extends TestCase
         ]);
 
     }
+
+    public function testCreateItemEmptyItemNameField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+            'itemName' => '',
+            'itemCode' => 'AAA1',
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemName[0] === 'The item name field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => '',
+            'itemCode' => 'AAA1',
+        ]);
+
+    }
+
+    public function testCreateItemEmptyItemCodeField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+            'itemName' => 'Sample Item',
+            'itemCode' => '',
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemCode[0] === 'The item code field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => 'Sample Item',
+            'itemCode' => '',
+        ]);
+
+    }
+
+    public function testCreateItemEmptyAllRequiredField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+            'itemName' => '',
+            'itemCode' => '',
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemCode[0] === 'The item code field is required.');
+        $this->assertTrue($getData->errors->itemName[0] === 'The item name field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => '',
+            'itemCode' => '',
+        ]);
+
+    }
+
+    //----
+    public function testCreateItemMissingItemNameField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+            'itemCode' => 'AAA1',
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemName[0] === 'The item name field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => '',
+            'itemCode' => 'AAA1',
+        ]);
+
+    }
+
+    public function testCreateItemMissingItemCodeField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+            'itemName' => 'Sample Item',
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemCode[0] === 'The item code field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => 'Sample Item',
+            'itemCode' => '',
+        ]);
+
+    }
+
+    public function testCreateItemMissingAllRequiredField(): void
+    {
+        $user = User::factory()->create([
+            'isAdmin' => true
+        ]);
+        $this->actingAs($user);
+        $itemController = new \App\Http\Controllers\Api\ItemController;
+        // Create a new Request object with valid data
+        $request = Request::create('/api/items', 'POST', [
+        ]);
+        $response = $itemController->store($request);
+        $getData = $response->getData();
+        $this->assertTrue($getData->errors->itemCode[0] === 'The item code field is required.');
+        $this->assertTrue($getData->errors->itemName[0] === 'The item name field is required.');
+        $this->assertDatabaseMissing('items', [
+            'itemName' => '',
+            'itemCode' => '',
+        ]);
+
+    }
 }
